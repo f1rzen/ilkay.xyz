b2d3e91b1770a78b7510d75ccbd1cab3fe724298
İlkay <ilkaybaytar@aol.com>
2023-12-30 17:10:42 +0300

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,21 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ['./src/**/*.{html,js,svelte,ts}'],
+  theme: {
+    extend: {
+      colors: {
+        green10: '#a7cec5',
+        green20: '#129f74',
+        yellow10: '#fff4d9',
+        yellow20: '#fcc556',
+        yellow30: '#f9d139',
+        purple10: '#be9def',
+        purple20: '#9d5b9c',
+        pink: '#fec4e2',
+        orange: '#d2641a',
+      },
+    },
+  },
+  plugins: [],
+}
+
